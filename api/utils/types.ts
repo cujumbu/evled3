fca810b8d530d8e48f25d7e9aff88a324a3571c6
cujumbu <nethandelskompagniet@gmail.com>
2024-12-10 16:54:05 +0100

    Update types.ts

--- api/utils/types.ts
+++ api/utils/types.ts
@@ -4,6 +4,7 @@ export interface TimerConfig {
   id: string;
   user_id: string;
   end_date: string;
+  timezone: string;
   style: 'modern' | 'minimal' | 'classic' | 'neon' | 'gradient' | 'elegant';
   color: string;
   language: Language;
